a73716d7f9ec2f59376d4945e8f837620e2c6377
Aryan Singh <71259822+ARYANSINGHR4T2@users.noreply.github.com>
2025-08-27 20:18:54 +0530

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,22 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ */
+
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("metro-react-native-babel-transformer")
+    },
+    resolver: {
+      assetExts,
+      sourceExts: [...sourceExts, "cjs"]
+    }
+  };
+})();
