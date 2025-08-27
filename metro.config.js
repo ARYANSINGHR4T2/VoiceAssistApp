371d7ef66251e96ecc2e3ca88fd6216d4ad93946
Aryan Singh <71259822+ARYANSINGHR4T2@users.noreply.github.com>
2025-08-27 20:27:59 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,14 @@
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
+ *
+ * @format
  */
-
-const { getDefaultConfig } = require("metro-config");
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("metro-react-native-babel-transformer")
-    },
-    resolver: {
-      assetExts,
-      sourceExts: [...sourceExts, "cjs"]
-    }
-  };
-})();
+module.exports = {
+  transformer: {
+    babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
+  },
+  resolver: {
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'],
+  },
+};
